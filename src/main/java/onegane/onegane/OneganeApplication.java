f9a811211721a452a0a4ca256d4ccf245ddabeb4
마현우 <mhw35689402@gmail.com>
2023-04-20 17:50:17 +0900

    init project

---
+++ src/main/java/onegane/onegane/OneganeApplication.java
@@ -0,0 +1,13 @@
+package onegane.onegane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneganeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneganeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/onegane/onegane/OneganeApplicationTests.java
@@ -0,0 +1,13 @@
+package onegane.onegane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneganeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
