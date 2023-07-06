531c8abfba03a992a3f435013bfa885640e932ef
마현우 <mhw35689402@gmail.com>
2023-07-07 00:14:52 +0900

    feat : User 도메인 구성

---
+++ src/main/java/onegane/onegane/domain/user/domain/User.java
@@ -0,0 +1,37 @@
+package onegane.onegane.domain.user.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer studentNumber;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer entranceYear;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(Integer studentNumber, String name, Integer entranceYear, Role role) {
+        this.studentNumber = studentNumber;
+        this.name = name;
+        this.entranceYear = entranceYear;
+        this.role = role;
+    }
+}
