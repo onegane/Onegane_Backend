3cc0045586344488a05be22e5feef5f25b9f1339
마현우 <mhw35689402@gmail.com>
2023-07-08 23:09:23 +0900

    refactor : 패키지 구조 변경

--- src/main/java/onegane/onegane/global/config/security/SecurityConfig.java
+++ src/main/java/onegane/onegane/global/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package onegane.onegane.global.security;
+package onegane.onegane.global.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
