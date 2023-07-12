bd25fec4461c3b857bdbbcd1971ca11280e279f4
baekseojin <seojinbaek100@gmail.com>
2023-07-12 20:02:26 +0900

    fix : 빈 주입이 안 되던 문제 수정

--- src/main/java/onegane/onegane/OneganeApplication.java
+++ src/main/java/onegane/onegane/OneganeApplication.java
@@ -2,10 +2,12 @@ package onegane.onegane;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackages = {"onegane.onegane.global.jwt", "onegane.onegane.global.config.security.auth"})
 public class OneganeApplication {
 
     public static void main(String[] args) {
