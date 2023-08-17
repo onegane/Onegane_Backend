39007daa50f901faf5fe17a51573a5c5200d337e
마현우 <mhw35689402@gmail.com>
2023-08-17 17:50:33 +0900

    fix: 제대로 작동하지 않던 빈주입 문제 해결

--- src/main/java/onegane/onegane/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/onegane/onegane/domain/auth/repository/RefreshTokenRepository.java
@@ -2,9 +2,11 @@ package onegane.onegane.domain.auth.repository;
 
 import onegane.onegane.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findByAccessToken(String accessToken);
