824e1c2ef0067e7c4a91d1af207b00e912ab305d
마현우 <mhw35689402@gmail.com>
2023-10-01 23:39:56 +0900

    refactor: HTTP 메소드 변경

--- src/main/java/onegane/onegane/domain/history/presentation/controller/CameraDataController.java
+++ src/main/java/onegane/onegane/domain/history/presentation/controller/CameraDataController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import onegane.onegane.domain.history.presentation.dto.CameraDataRequest;
 import onegane.onegane.domain.history.service.CameraDataService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +16,8 @@ public class CameraDataController {
 
     private final CameraDataService cameraDataService;
 
-    @PostMapping
-    public ResponseEntity<?> saveTrackingNumber(@RequestBody CameraDataRequest request) {
+    @PutMapping
+    public ResponseEntity<?> updateHistoryState(@RequestBody CameraDataRequest request) {
         return ResponseEntity.ok(cameraDataService.save(request));
     }
 }
