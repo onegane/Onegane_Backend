85acbb32bba59c1f703d8d9bc5482812d472b746
마현우 <mhw35689402@gmail.com>
2023-10-01 22:44:51 +0900

    refactor: 저장할 필요가 없어서 삭제

--- src/main/java/onegane/onegane/domain/history/repository/CameraDataRepository.java
+++
@@ -1,9 +0,0 @@
-package onegane.onegane.domain.history.repository;
-
-import onegane.onegane.domain.history.domain.CameraData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CameraDataRepository extends JpaRepository<CameraData, String> {
-}
