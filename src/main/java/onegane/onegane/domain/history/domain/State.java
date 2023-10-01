d84f21b1a8794cf9ff498a553aaf112e2eb2ce32
마현우 <mhw35689402@gmail.com>
2023-10-01 22:50:10 +0900

    refactor: 상태 추가

--- src/main/java/onegane/onegane/domain/history/domain/State.java
+++ src/main/java/onegane/onegane/domain/history/domain/State.java
@@ -2,5 +2,16 @@ package onegane.onegane.domain.history.domain;
 
 public enum State {
     NOT_ARRIVED,
-    ARRIVED
+    STATE_11,
+    STATE_12,
+    STATE_13,
+    STATE_14,
+    STATE_21,
+    STATE_22,
+    STATE_23,
+    STATE_24,
+    STATE_31,
+    STATE_32,
+    STATE_33,
+    STATE_34,
 }
