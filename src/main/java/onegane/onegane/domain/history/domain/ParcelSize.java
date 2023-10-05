cac3c57510a31565c6f3fc9b653c49da746089b1
마현우 <mhw35689402@gmail.com>
2023-10-06 00:22:40 +0900

    feat: 택배 사이즈 저장하는 엔티티 추가

---
+++ src/main/java/onegane/onegane/domain/history/domain/ParcelSize.java
@@ -0,0 +1,38 @@
+package onegane.onegane.domain.history.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ParcelSize {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "history_id", unique = true)
+    private History history;
+
+    @Column(nullable = false)
+    private Integer x;
+
+    @Column(nullable = false)
+    private Integer y;
+
+    @Column(nullable = false)
+    private Integer z;
+
+    @Builder
+    public ParcelSize(History history, Integer x, Integer y, Integer z) {
+        this.history = history;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
---
+++ src/main/java/onegane/onegane/domain/history/repository/ParcelSizeRepository.java
@@ -0,0 +1,7 @@
+package onegane.onegane.domain.history.repository;
+
+import onegane.onegane.domain.history.domain.ParcelSize;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParcelSizeRepository extends JpaRepository<ParcelSize, Long> {
+}
