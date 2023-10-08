44d278c58e1b4534b0f11291cf7bb55040ddb1eb
마현우 <mhw35689402@gmail.com>
2023-10-08 17:01:53 +0900

    feat: findByHistory 메서드 추가

--- src/main/java/onegane/onegane/domain/history/repository/ParcelSizeRepository.java
+++ src/main/java/onegane/onegane/domain/history/repository/ParcelSizeRepository.java
@@ -1,7 +1,10 @@
 package onegane.onegane.domain.history.repository;
 
+import onegane.onegane.domain.history.domain.History;
 import onegane.onegane.domain.history.domain.ParcelSize;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParcelSizeRepository extends JpaRepository<ParcelSize, Long> {
+
+    ParcelSize findByHistory(History history);
 }
